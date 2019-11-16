3fb252afacb57394bc9e723e475b00244cef187a
FahRamatis <fah.ramatis@gmail.com>
2019-11-16 19:44:59 -0300

    Tests for contacts

--- __tests__/integration/contacts.test.js
+++ __tests__/integration/contacts.test.js
@@ -2,12 +2,13 @@ import request from 'supertest'
 import app from '../../src/app'
 import factory from '../factories'
 import roles from '../../src/config/roles'
-
-let userID = 0
-let token = ''
+import truncate from '../utils/truncate'
 
 describe('Contacts', () => {
+  let userID = 0
+  let token = ''
   beforeEach(async () => {
+    await truncate()
     let userRegistered
     let userToken
     let user = await factory.attrs('User', {
@@ -31,12 +32,12 @@ describe('Contacts', () => {
   })
 
   it('should be able to store contacts', async () => {
+    const authStr = 'Bearer ' + token
     const contact = {
       email: 'teste@teste.com',
       phone: 19974154036,
-      cellphone: 19974154036,
+      cellphone: 19974154036
     }
-    const authStr = 'Bearer ' + token
     const response = await request(app)
       .post('/contacts')
       .set('Authorization', authStr)
@@ -47,6 +48,16 @@ describe('Contacts', () => {
 
   it('should be able to list contacts', async () => {
     const authStr = 'Bearer ' + token
+    const contact = {
+      email: 'teste@teste.com',
+      phone: 19974154036,
+      cellphone: 19974154036
+    }
+
+    await request(app)
+      .post('/contacts')
+      .set('Authorization', authStr)
+      .send({ ...contact })
 
     const response = await request(app)
       .get('/contacts')
@@ -58,6 +69,17 @@ describe('Contacts', () => {
   it('should be able to show a single contact', async () => {
     const authStr = 'Bearer ' + token
 
+    const contact = {
+      email: 'teste@teste.com',
+      phone: 19974154036,
+      cellphone: 19974154036
+    }
+
+    await request(app)
+      .post('/contacts')
+      .set('Authorization', authStr)
+      .send({ ...contact })
+
     const response = await request(app)
       .get(`/bankData/${userID}`)
       .set('Authorization', authStr)
@@ -66,12 +88,24 @@ describe('Contacts', () => {
   })
 
   it('should be able to update contact', async () => {
+    const authStr = 'Bearer ' + token
+    const initialContact = {
+      email: 'teste@teste.com',
+      phone: 19974154036,
+      cellphone: 19974154036
+    }
+
+    await request(app)
+      .post('/contacts')
+      .set('Authorization', authStr)
+      .send({ ...initialContact })
+
     const contact = {
       email: 'teste_modified@teste.com',
       phone: 19974312312,
-      cellphone: 19974312312,
+      cellphone: 19974312312
     }
-    const authStr = 'Bearer ' + token
+
     const response = await request(app)
       .put(`/contacts/${userID}`)
       .set('Authorization', authStr)
@@ -83,10 +117,25 @@ describe('Contacts', () => {
   it('should be able to delete a single contact', async () => {
     const authStr = 'Bearer ' + token
 
+    const contact = {
+      email: 'teste@teste.com',
+      phone: 19974154036,
+      cellphone: 19974154036
+    }
+
+    await request(app)
+      .post('/contacts')
+      .set('Authorization', authStr)
+      .send({ ...contact })
+
     const response = await request(app)
       .delete(`/contacts/${userID}`)
       .set('Authorization', authStr)
 
     expect(response.body).toHaveProperty('id')
+  })
+
+  afterAll(async () => {
+    await truncate()
   })
 })
