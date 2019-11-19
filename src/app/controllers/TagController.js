ffeb4992b66cd14b40f483fd11a9db075c90cf56
FahRamatis <fah.ramatis@gmail.com>
2019-11-18 21:02:02 -0300

    tag controller

--- src/app/controllers/TagController.js
+++ src/app/controllers/TagController.js
@@ -2,19 +2,21 @@ import Tag from '../models/Tag.js'
 import * as Yup from 'yup'
 
 class TagController {
-  async index (req, res) {
+  async index(req, res) {
     Tag.findAll().then(data => {
       return res.json(data)
     })
   }
 
-  async store (req, res) {
+  async store(req, res) {
     const schema = Yup.object().shape({
-      note: Yup.number().integer().required(),
+      note: Yup.number()
+        .integer()
+        .required(),
       comment: Yup.string()
     })
 
-    if (!await schema.isValid(req.body)) {
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' })
     }
 
@@ -23,31 +25,36 @@ class TagController {
       return res.status(400).json({ error: 'Could not store record.' })
     }
 
-    const tag = await Tag.create(req.body)
+    const tag = await Tag.create({
+      ...req.body,
+      user_id: req.userId
+    })
+
     return res.json(tag)
   }
 
-  async show (req, res) {
+  async show(req, res) {
     const tag = await Tag.findOne({ where: { user_id: req.params.id } })
     return res.json(tag)
   }
 
-  async update (req, res) {
+  async update(req, res) {
     const schema = Yup.object().shape({
       note: Yup.number().integer(),
       comment: Yup.string()
     })
 
-    if (!await schema.isValid(req.body)) {
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' })
     }
 
     const tag = await Tag.findOne({ where: { user_id: req.params.id } })
+    if (!tag) return res.status(400).json({ error: 'Tag doenst exists' })
     tag.update(req.body, { where: { user_id: req.params.id } })
     return res.json(tag)
   }
 
-  async destroy (req, res) {
+  async destroy(req, res) {
     Tag.destroy({ where: { user_id: req.params.id } }).then(deleted => {
       if (deleted) {
         return res.json({ success: 'Deleted successfully' })
